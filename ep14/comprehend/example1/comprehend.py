ed3f1a7af92f06d1de772bc2ca73f38a1acb4122
Rahul Subramaniam <rahul.subramaniam@devfactory.com>
2022-08-09 14:31:56 +0000

    initial commit for ep14

---
+++ ep14/comprehend/example1/comprehend.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+
+client = boto3.client('comprehend', region_name='us-east-1')
+with open('raw_text.txt') as raw_text:
+    lines = raw_text.readlines()
+
+def detect_entities(str):
+    return client.detect_entities(
+        Text=str,
+        LanguageCode='en'
+    )
+
+def detect_key_phrases(str):
+    return client.detect_key_phrases(
+    Text=str,
+    LanguageCode='en'
+)
+
+# response = detect_entities("\n".join(lines))
+response = detect_key_phrases("\n".join(lines))
+print(json.dumps(response, indent=3))
